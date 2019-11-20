419ea66b36c0b236fad8c7c96b9f39c96e0ca6fe
Kate F <follett.katie@gmail.com>
2019-11-20 01:17:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const obj = { foo: "bar" };
- 
-const newObj = Object.assign({}, obj);
- 
-newObj;
-// => { foo: "bar" }
- 
-delete newObj['foo'];
-// => true
- 
-newObj;
-// => {}
- 
-obj;
-// => { foo: "bar" }
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  // Alternate using ES6 Spread operators:
+  // return { ...driver, ...{ [key]: value } }
+  return Object.assign({}, driver, { [key]: value });
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+
+  return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+  // Alternate using ES6 Spread operators:
+  // const newObj = { ...driver }
+  const newObj = Object.assign({}, driver);
+
+  delete newObj[key];
+
+  return newObj;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+
+  return driver;
+}
